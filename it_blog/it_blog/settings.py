d15c9cc130fc55e7a810ac0460d3612f17379f2b
vadimkondratovich <vadim.kondratovich1993@gmail.com>
2021-09-09 21:58:45 +0300

    User_model

--- it_blog/it_blog/settings.py
+++ it_blog/it_blog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
     'home',
     "blog",
 ]
@@ -134,3 +135,4 @@ STATICFILES_DIRS = [BASE_DIR / "it_blog" / "static"]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = "user.USER"
