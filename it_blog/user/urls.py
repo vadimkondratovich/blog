a9715e5f09251170c6ace7e094b1ea702f41a75f
Vadim <vadim.kondratovich1993@gmail.com>
2021-09-25 15:38:23 +0300

    forms_views_urls

--- it_blog/user/urls.py
+++ it_blog/user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from user.views import signin
+from user.views import signin, login
+from django.shortcuts import redirect
 
 urlpatterns = [
     path("signin/", signin, name="signin_page"),
-    #path("login/", signin, name="login_page"),
+    path("login/", signin, name="login_page"),
     #path("", lambda request: redirect("/user/login/")),
 ]
