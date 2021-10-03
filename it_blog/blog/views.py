3155e4329dcadaee70b08ff28cb563d2ca4ce890
Vadim <vadim.kondratovich1993@gmail.com>
2021-10-03 22:53:15 +0300

    single_blog

--- it_blog/blog/views.py
+++ it_blog/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseForbidden
 from blog.forms import CreatePost
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from blog.models import Post
 from django.views.generic.edit import FormView
 
@@ -16,13 +16,17 @@ class AllPosts(ListView):
     model = Post
     template_name = "all_blogs.html"
 
-
+def single_blog(request, pk):
+    context = {"post": Post.objects.get(id=pk)}
+    context["post"].views += 1
+    context["post"].save()
+    return render(request, "single_blog.html", context)
 
 
 class NewPost(LoginRequiredMixin, FormView):
     template_name = "create_blog.html"
     form_class = CreatePost
-    login_url = '/user/login/'
+    login_url = "/user/login/"
     success_url = "/blog/"
 
     def form_valid(self, form):
